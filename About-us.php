b01b4f8ce78c5af0f54928c68468eb90ddfc479d
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2020-06-20 09:47:22 +0530

    about-us file

--- About-us.php
+++ About-us.php
@@ -6,9 +6,8 @@
 </head>
 
 <body>
-
     <div class="container-fluid bg-dark">
-        <?php $page='about-us'; include 'includes/navbar_part.php'; ?>
+        <?php $page='about-us'; include 'includes/navbar_part.php';?>
     </div>
     <div class="row mt-5 container">
         <div class="col-12 text-center mt-5">
@@ -16,54 +15,54 @@
         </div>
     </div>
 
-    <section class="about-us-part">
-        <div class="container">
-            <h2 class="demo-head-text">View PHP-Framework Developer</h2>
-                <p class="discription-text">Differet type of Php Developers are available here.</p>
-            <div class="row line-part">
-                <div class="intro-one">
-                    <div class="d-flex mt-5">
-                        <img src="./images/person-1.gif" class="person-image">
-                        <div class="info-part p-2 ml-2">
-                            <h4 class="info-1">Lorem ipsum</h4>
+        <section class="about-us-part">
+            <div class="container">
+                <h2 class="demo-head-text">View PHP-Framework Developer</h2>
+                    <p class="discription-text">Differet type of Php Developers are available here.</p>
+                <div class="row line-part">
+                    <div class="intro-one">
+                        <div class="d-flex mt-5">
+                            <img src="./images/person-1.gif" class="person-image">
+                            <div class="info-part p-2 ml-2">
+                                <h4 class="info-1">Lorem ipsum</h4>
                                 <p class="talent-text">Php Developer</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="intro-one">
-                    <div class="d-flex mt-5">
-                        <img src="./images/person-1.gif" class="person-image">
-                        <div class="info-part p-2 ml-2">
-                            <h4 class="info-1">Lorem ipsum</h4>
+                    <div class="intro-one">
+                        <div class="d-flex mt-5">
+                            <img src="./images/person-1.gif" class="person-image">
+                            <div class="info-part p-2 ml-2">
+                                <h4 class="info-1">Lorem ipsum</h4>
                                 <p class="talent-text">Laravel Developer</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="intro-one">
-                    <div class="d-flex mt-5">
-                        <img src="./images/person-1.gif" class="person-image">
-                        <div class="info-part p-2 ml-2">
-                            <h4 class="info-1">Lorem ipsum</h4>
+                    <div class="intro-one">
+                        <div class="d-flex mt-5">
+                            <img src="./images/person-1.gif" class="person-image">
+                            <div class="info-part p-2 ml-2">
+                                <h4 class="info-1">Lorem ipsum</h4>
                                 <p class="talent-text">Magento Developer</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-            </div>
-                <p class="details-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore illum, temporibus veritatiseligendi, 
-                    aliquam, dolor enim itaque veniam aut eaque sequi qui quia vitae pariatur repudiandae ab dignissimos ex!
-                </p>
-                <button type="button" class="btn btn-secondary submit-part-btn">Get more Information</button>
-        </div>
-   </section>
+        <p class="details-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam dolore illum, temporibus veritatis
+     eligendi, 
+    aliquam, dolor enim itaque veniam aut eaque sequi qui quia vitae pariatur repudiandae ab dignissimos ex!</p>
+    <button type="button" class="btn btn-secondary submit-part-btn">Get more Information</button>
+</div>
+</section>
 
 <!--Footer-part start here-->
-    <?php  include 'includes/footer_part.php'; ?>
+<?php  include 'includes/footer_part.php';?>
     <!--footer-part end here-->
     <!--script-part-->
-    <?php  include 'includes/script.php';?>
-    
+<?php  include 'includes/script.php';?>
 </body>
+
+
 </html>
