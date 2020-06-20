831fcf433a29337848eb97b607e31a5d1d911861
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2020-06-20 09:47:56 +0530

    fisrt-database contact file

--- Contact-us.php
+++ Contact-us.php
@@ -19,37 +19,37 @@
     <section class="contact-part">
         <div class="container">
             <h2 class="demo-head-text">Contact with Expert Person</h2>
-            <p class="discription-text">Some programmimg language experts are always here for you.</p>
-            <div class="form-part">
-                <form action="userinfo.php">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" name="first-name" aria-describedby="emailHelp"
-                                placeholder="Enter First Name">
+                <p class="discription-text">Some programmimg language experts are always here for you.</p>
+                <div class="form-part">
+                    <form action="userinfo.php">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="first-name" aria-describedby="emailHelp"
+                                    placeholder="Enter First Name">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="last-name" aria-describedby="nameHelp"
+                                    placeholder="Enter Last Name">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" aria-describedby="nameHelp"
+                                    placeholder="Enter Email">
+                            </div>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="phone" aria-describedby="nameHelp"
+                                    placeholder="Enter Phone No">
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" name="last-name" aria-describedby="nameHelp"
-                                placeholder="Enter Last Name">
+                        <textarea class="form-control" name="message" placeholder="Enter Your Message"
+                            rows="4"></textarea>
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-4">
+                                <button type="submit" class="btn btn-secondary submit-part-btn">Send Message</button>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                            <input type="email" class="form-control" name="email" aria-describedby="nameHelp"
-                                placeholder="Enter Email">
-                        </div>
-                        <div class="col-md-6">
-                            <input type="text" class="form-control" name="phone" aria-describedby="nameHelp"
-                                placeholder="Enter Phone No">
-                        </div>
-                    </div>
-                    <textarea class="form-control" name="message" placeholder="Enter Your Message"
-                        rows="4"></textarea>
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4">
-                            <button type="submit" class="btn btn-secondary submit-part-btn">Send Message</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
         </div>
 
     </section>
